581b03ccd64ba153b9e584a26bfa3fe5a969795b
Thomas VARLET <thovarMS@users.noreply.github.com>
2016-10-21 11:27:39 +0200

    showmount is not using a hardcoded ip for jumpbox
    WAS:
       showmount -e 10.0.0.4
    NOW:
      showmount -e $IPPRE

--- RawCluster/cn-setup.sh
+++ RawCluster/cn-setup.sh
@@ -21,7 +21,7 @@ systemctl start nfs-idmap
 localip=`hostname -i | cut --delimiter='.' -f -3`
 echo "$IPPRE:/mnt/nfsshare    /mnt/nfsshare   nfs defaults 0 0" | tee -a /etc/fstab
 echo "$IPPRE:/mnt/resource/scratch    /mnt/scratch   nfs defaults 0 0" | tee -a /etc/fstab
-showmount -e 10.0.0.4
+showmount -e $IPPRE
 mount -a
 
 ln -s /opt/intel/impi/5.1.3.181/intel64/bin/ /opt/intel/impi/5.1.3.181/bin
