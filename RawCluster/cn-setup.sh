e5d26fe52de6a6c9f5ed3b27a7337f8696c7d1dc
Thomas VARLET <thovarMS@users.noreply.github.com>
2016-11-14 15:40:10 -0700

    Add SLES distro

--- RawCluster/cn-setup.sh
+++ RawCluster/cn-setup.sh
@@ -4,6 +4,7 @@ echo ############# Compute Node Setup #################
 echo ##################################################
 IPPRE=$1
 SKUNAME=$2
+LXDISTRO=$3
 USER=`whoami`
 yum install -y -q nfs-utils
 mkdir -p /mnt/nfsshare
@@ -11,23 +12,24 @@ mkdir -p /mnt/nfsshare
 mkdir -p /mnt/scratch
 chmod 777 /mnt/nfsshare
 chmod 777 /mnt/scratch
-if [ "$SKUNAME" == "6.5" ] ; then
-# For CentOS 6.5 (systemctl is supported for version > 7)
-   chkconfig nfs on 
-   chkconfig rpcbind on 
-   service rpcbind start
-   service nfs start
-else
-   systemctl enable rpcbind
-   systemctl enable nfs-server
-   systemctl enable nfs-lock
-   systemctl enable nfs-idmap
-   systemctl start rpcbind
-   systemctl start nfs-server
-   systemctl start nfs-lock
-   systemctl start nfs-idmap
-fi
-   
+if [ "$LXDISTRO" == "CentOS-HPC" ] ; then
+   if [ "$SKUNAME" == "6.5" ] ; then
+   # For CentOS 6.5 (systemctl is supported for version > 7)
+      chkconfig nfs on 
+      chkconfig rpcbind on 
+      service rpcbind start
+      service nfs start
+   else
+      systemctl enable rpcbind
+      systemctl enable nfs-server
+      systemctl enable nfs-lock
+      systemctl enable nfs-idmap
+      systemctl start rpcbind
+      systemctl start nfs-server
+      systemctl start nfs-lock
+      systemctl start nfs-idmap
+   fi
+fi   
 localip=`hostname -i | cut --delimiter='.' -f -3`
 echo "$IPPRE:/mnt/nfsshare    /mnt/nfsshare   nfs defaults 0 0" | tee -a /etc/fstab
 echo "$IPPRE:/mnt/resource/scratch    /mnt/scratch   nfs defaults 0 0" | tee -a /etc/fstab
